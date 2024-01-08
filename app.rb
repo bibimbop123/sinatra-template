eb2107d37a1cc1245fbfefcc678b4d5ec7bd4c7d
Brian Kim <brianhkim13@gmail.com>
2024-01-08 23:57:32 +0000

    Metropolitan Museum of Art Collection

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
+require 'dotenv/load'
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get("/") do 
+  url = "https://collectionapi.metmuseum.org/public/collection/v1/departments"
+  response = HTTP.get(url)
+  
+  if response.body.empty?
+    @data = "No response received from API"
+  else
+    begin
+      @data = JSON.parse(response.body)
+    rescue JSON::ParserError
+      @data = "Response is not valid JSON"
+    end
+  end
+  erb(:home)
 end
