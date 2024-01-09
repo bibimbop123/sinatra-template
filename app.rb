fa2b368fef0ab4e36658aa816f67e79230916952
Brian Kim <brianhkim13@gmail.com>
2024-01-09 20:47:49 +0000

    department_id route

--- app.rb
+++ app.rb
@@ -12,10 +12,28 @@ get("/") do
     @data = "No response received from API"
   else
     begin
-      @data = JSON.parse(response.body)
+      @data = JSON.parse(response.body).fetch("departments")
     rescue JSON::ParserError
       @data = "Response is not valid JSON"
     end
   end
   erb(:home)
+end
+
+get("/:department_id") do
+  @id = params.fetch("department_id")
+
+  url = "https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=#{@id}"
+  response = HTTP.get(url)
+ 
+  if response.body.empty?
+    @data = "No response received from API"
+  else
+    begin
+      @data = JSON.parse(response.body)
+    rescue JSON::ParserError
+      @data = "Response is not valid JSON"
+    end
+  end
+  erb(:departments)
 end
